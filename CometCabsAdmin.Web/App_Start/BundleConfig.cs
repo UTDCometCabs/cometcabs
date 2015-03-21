82d738599f01ba0cccccc5029800b43b73939131
hendrasuteja <hendra.suteja@gmail.com>
2015-03-21 16:16:12 -0500

    Added route management view

--- CometCabsAdmin.Web/App_Start/BundleConfig.cs
+++ CometCabsAdmin.Web/App_Start/BundleConfig.cs
@@ -6,6 +6,10 @@ namespace CometCabsAdmin.Web
     {
         public static void RegisterBundles(BundleCollection bundles)
         {
+            bundles.Add(new StyleBundle("~/bundles/styles")
+                .Include("~/Content/Site.css")
+                );
+
             bundles.Add(new StyleBundle("~/bundles/bootstrap")
                 .Include("~/Content/bootstrap.css")
                 .Include("~/Content/bootstrap-theme.css")
@@ -15,7 +19,7 @@ namespace CometCabsAdmin.Web
                 .IncludeDirectory("~/Content/themes/base", "*.css")
                 );
 
-            bundles.Add(new ScriptBundle("~/bundles/script")
+            bundles.Add(new ScriptBundle("~/bundles/scripts")
                 .Include("~/Scripts/jquery-1.9.1.js")
                 .Include("~/Scripts/jquery-ui-1.11.3.js")
                 .Include("~/Scripts/angular-1.3.14.js")
---
+++ CometCabsAdmin.Web/Controllers/MapController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CometCabsAdmin.Web.Controllers
+{
+    [CometCabsAuthorize(Roles="Admin")]
+    public class MapController : Controller
+    {
+        private float _longitude;
+        private float _latitude;
+
+        // GET: Map
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
