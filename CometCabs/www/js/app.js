11ff9db799aed06e205c5c7be1e0f5655e2a9b96
Sean <sdyer@utdallas.edu>
2015-04-06 18:54:15 -0500

    Moved to using JSON for describing cabs, routes
    Added JSON to both describe cabs and routes. The cab markers and route
    lines are now drawn by extracting JSON from objects. This paves the way
    to receiving JSON from the server and using that.

--- CometCabs/www/js/app.js
+++ CometCabs/www/js/app.js
@@ -25,7 +25,7 @@ angular.module('starter', ['ionic'])
 	function initialize() {
 		/*Latitude and longitude for the school. Don't know whether we could use more precision */
         var site = new google.maps.LatLng(32.986,-96.750);
-		setRouteCoordinates();
+		
       
         var mapOptions = {
           streetViewControl:true,
@@ -35,8 +35,7 @@ angular.module('starter', ['ionic'])
 		/*The element references the div id in index.html*/
         map = new google.maps.Map(document.getElementById("map"),
             mapOptions);
-        
-		setRouteLines();
+        setRouteCoordinates(); //Sets the details for routes
 		setCabMarkers();
 		
 		/*Sets a marker for the current position */
@@ -60,161 +59,191 @@ angular.module('starter', ['ionic'])
   }
   
   function setRouteCoordinates() {
-	mcDermottCoords = [
-		new google.maps.LatLng(32.990034, -96.744273),			
-		new google.maps.LatLng(32.989341, -96.745367),
-		new google.maps.LatLng(32.988540, -96.745046),
-		new google.maps.LatLng(32.987919, -96.746194),
-		new google.maps.LatLng(32.987982, -96.746966),
-		new google.maps.LatLng(32.987361, -96.747020),
-		new google.maps.LatLng(32.987487, -96.746172),
-		new google.maps.LatLng(32.984653, -96.745990),
-		new google.maps.LatLng(32.984635, -96.745367),
-		new google.maps.LatLng(32.985616, -96.745325),
-		new google.maps.LatLng(32.985616, -96.745958),
-		new google.maps.LatLng(32.987829, -96.746204),
-		new google.maps.LatLng(32.988405, -96.744938),			
-		new google.maps.LatLng(32.988747, -96.745046),			
-		new google.maps.LatLng(32.989359, -96.743908),		
-		new google.maps.LatLng(32.990034, -96.744273)
-	];
-	commonsCoords = [
-		new google.maps.LatLng(32.985645, -96.74914),			
-		new google.maps.LatLng(32.985666, -96.750987),
-		new google.maps.LatLng(32.990643, -96.750946),
-		new google.maps.LatLng(32.990721, -96.753669),
-		new google.maps.LatLng(32.991789, -96.753631),
-		new google.maps.LatLng(32.991789, -96.752607),
-		new google.maps.LatLng(32.990734, -96.752620),
-		new google.maps.LatLng(32.990721, -96.750949),
-		new google.maps.LatLng(32.990643, -96.750946)			
-	];	
+	/*
+		The JSON for routes:
+		var jsonRoute = {
+			"name": <string>,
+			"color": <in the form #000000>,
+			[
+			{"latitude": <double>, "longitude": <double> } ]
+		}	
+			With two or more points in the array.	
+	*/
 	
-	phase1NorthCoords = [
-		new google.maps.LatLng(32.985645, -96.74914),			
-		new google.maps.LatLng(32.985668, -96.754610),
-		new google.maps.LatLng(32.985850, -96.754610),
-		new google.maps.LatLng(32.985864, -96.753907),
-		new google.maps.LatLng(32.986955, -96.753751),
-		new google.maps.LatLng(32.987130, -96.753778),
-		new google.maps.LatLng(32.987124, -96.753910),
-		new google.maps.LatLng(32.988195, -96.753947),
-		new google.maps.LatLng(32.988219, -96.755001),
-		new google.maps.LatLng(32.988388, -96.755001),
-		new google.maps.LatLng(32.988368, -96.753725),
-		new google.maps.LatLng(32.985668, -96.753741)		
+	var routes = [
+		{
+			"name": "McDermott",
+			"color": '#003986',
+			"path": [
+				{"latitude": 32.990034, "longitude": -96.744273},
+				{"latitude": 32.989341, "longitude": -96.745367},
+				{"latitude": 32.988540, "longitude": -96.745046},
+				{"latitude": 32.987919, "longitude": -96.746194},
+				{"latitude": 32.987982, "longitude": -96.746966},
+				{"latitude": 32.987361, "longitude": -96.747020},
+				{"latitude": 32.987487, "longitude": -96.746172},
+				{"latitude": 32.984653, "longitude": -96.745990},
+				{"latitude": 32.984635, "longitude": -96.745367},
+				{"latitude": 32.985616, "longitude": -96.745325},
+				{"latitude": 32.985616, "longitude": -96.745958},
+				{"latitude": 32.987829, "longitude": -96.746204},
+				{"latitude": 32.988405, "longitude": -96.744938},			
+				{"latitude": 32.988747, "longitude": -96.745046},			
+				{"latitude": 32.989359, "longitude": -96.743908},		
+				{"latitude": 32.990034, "longitude": -96.744273}			
+			]
+		},
+		{
+			"name": "Commons",
+			"color": '#9978c8',
+			"path": [
+				{"latitude":32.985645, "longitude": -96.74914},		
+				{"latitude":32.985666, "longitude": -96.750987},
+				{"latitude":32.990643, "longitude": -96.750946},
+				{"latitude":32.990721, "longitude": -96.753669},
+				{"latitude":32.991789, "longitude": -96.753631},
+				{"latitude":32.991789, "longitude": -96.752607},
+				{"latitude":32.990734, "longitude": -96.752620},
+				{"latitude":32.990721, "longitude": -96.750949},
+				{"latitude":32.990643, "longitude": -96.750946}				
+			]
+		},
+		{
+			"name": "Phase 1 North",
+			"color": '#FF9900',
+			"path": [
+				{"latitude":32.985645, "longitude":-96.74914},			
+				{"latitude":32.985668, "longitude":-96.754610},
+				{"latitude":32.985850, "longitude":-96.754610},
+				{"latitude":32.985864, "longitude":-96.753907},
+				{"latitude":32.986955, "longitude":-96.753751},
+				{"latitude":32.987130, "longitude":-96.753778},
+				{"latitude":32.987124, "longitude":-96.753910},
+				{"latitude":32.988195, "longitude":-96.753947},
+				{"latitude":32.988219, "longitude":-96.755001},
+				{"latitude":32.988388, "longitude":-96.755001},
+				{"latitude":32.988368, "longitude":-96.753725},
+				{"latitude":32.985668, "longitude":-96.753741}			
+			]
+		},
+		{
+			"name": "Phase 1 South",
+			"color": '#FF33FF',
+			"path": [
+				{"latitude":32.985647, "longitude":-96.74914},			
+				{"latitude":32.985672, "longitude":-96.754277},
+				{"latitude":32.985456, "longitude":-96.754309},
+				{"latitude":32.985035, "longitude":-96.753988},
+				{"latitude":32.983748, "longitude":-96.754399},
+				{"latitude":32.983750, "longitude":-96.755866},
+				{"latitude":32.985345, "longitude":-96.755054},
+				{"latitude":32.985664, "longitude":-96.755254},
+				{"latitude":32.985672, "longitude":-96.754277}			
+			]
+		},
+		{
+			"name": "Rutford Rd",
+			"color": '#005710',
+			"path": [
+				{"latitude":32.981273, "longitude":-96.750881},
+				{"latitude":32.981273, "longitude":-96.750190},
+				{"latitude":32.980547, "longitude":-96.750190},
+				{"latitude":32.980504, "longitude":-96.750881},
+				{"latitude":32.98422, "longitude":-96.751053},
+				{"latitude":32.992023, "longitude":-96.750967},
+				{"latitude":32.992068, "longitude":-96.752984},
+				{"latitude":32.992768, "longitude":-96.753057},
+				{"latitude":32.992757, "longitude":-96.751475},
+				{"latitude":32.992068, "longitude":-96.751461}	
+			]
+		}
 	];
+
+		
+	for (i = 0; i < routes.length; i++) {
+		setRouteFromJSON(routes[i]);
+	}
 	
-	phase1SouthCoords = [
-		new google.maps.LatLng(32.985647, -96.74914),			
-		new google.maps.LatLng(32.985672, -96.754277),
-		new google.maps.LatLng(32.985456, -96.754309),
-		new google.maps.LatLng(32.985035, -96.753988),
-		new google.maps.LatLng(32.983748, -96.754399),
-		new google.maps.LatLng(32.983750, -96.755866),
-		new google.maps.LatLng(32.985345, -96.755054),
-		new google.maps.LatLng(32.985664, -96.755254),
-		new google.maps.LatLng(32.985672, -96.754277)			
-	];
-	
-	rutfordCoords = [
-		new google.maps.LatLng(32.981273, -96.750881),
-		new google.maps.LatLng(32.981273, -96.750190),
-		new google.maps.LatLng(32.980547, -96.750190),
-		new google.maps.LatLng(32.980504, -96.750881),
-		new google.maps.LatLng(32.98422, -96.751053),
-		new google.maps.LatLng(32.992023, -96.750967),
-		new google.maps.LatLng(32.992068, -96.752984),
-		new google.maps.LatLng(32.992768, -96.753057),
-		new google.maps.LatLng(32.992757, -96.751475),
-		new google.maps.LatLng(32.992068, -96.751461)
-	];
   }
-  
-  function setRouteLines() {
-	rutfordRoute = new google.maps.Polyline({
-		path: rutfordCoords,
-		geodesic: true, 
-		strokeColor: '#005710',
+  function setRouteFromJSON(route) {
+	var routePath = [];
+	for (j = 0; j < route.path.length; j++) {
+		routePath.push(new google.maps.LatLng(route.path[j].latitude, route.path[j].longitude));
+	}
+	var routeLine = new google.maps.Polyline({
+		path: routePath,
+		geodesic: true,
+		strokeColor: route.color,
 		strokeOpacity: 1.0,
 		strokeWeight: 2.5,
-		map: map				
-	});
-		
-	phase1SouthRoute = new google.maps.Polyline({
-		path: phase1SouthCoords,
-		geodesic: true, 
-		strokeColor: '#FF33FF',
-		strokeOpacity: 1.0,
-		strokeWeight: 2.5,	
 		map: map
 	});
 	
-	phase1NorthRoute = new google.maps.Polyline({
-		path: phase1NorthCoords,
-		geodesic: true, 
-		strokeColor: '#FF9900',
-		strokeOpacity: 0.5,
-		strokeWeight: 2.5,	
-		map: map
-	});
-
-	commonsRoute = new google.maps.Polyline({
-		path: commonsCoords,
-		geodesic: true, 
-		strokeColor: '#9978c8',
-		strokeOpacity: 0.5,
-		strokeWeight: 2.5,	
-		map: map
-	});
-	
-	mcDermottRoute = new google.maps.Polyline({
-		path: mcDermottCoords,
-		geodesic: true, 
-		strokeColor: '#003986',
-		strokeOpacity: 1.0,
-		strokeWeight: 2.5,	
-		map: map
-	});
   }
 
+
+  function setCabColor(isFull, onDuty) {
+	var color = "#008000" ;
+	if (isFull) {
+		color = "#FFFF00";
+	}
+	if (!onDuty) {
+		color = "#FF0000";
+	}
+	return color;
+  }
+  
   function setCabMarkers() {
   		/*Code to add a marker representing a single icon to represent a cab.
 		The icon does not resize, so I'll have to add some sort of zoom listener
 		Will probably use http://stackoverflow.com/questions/3281524/resize-markers-depending-on-zoom-google-maps-v3
 		as a reference for that*/
-	rutfordMarker = new google.maps.Marker({
+	
+	/* The JSON for these should be:
+		var jsonCab = {
+			"latitude": <double>
+			"longitude": <double>
+			"isFull": <true or false>
+			"onDuty": <true or false>		
+		}
+		
+	*/
+	var cabs = [
+		{
+			"latitude": 32.986498,
+			"longitude": -96.751010,
+			"isFull": false,
+			"onDuty": true
+		},
+		{
+			"latitude": 32.985672,
+			"longitude": -96.754399,
+			"isFull": false,
+			"onDuty": false
+		}	
+	
+	];
+	
+	for (i = 0; i < cabs.length; i++) {
+		drawCab(cabs[i].latitude, cabs[i].longitude, cabs[i].isFull, cabs[i].onDuty);
+	}
+
+  }
+  
+  function drawCab(latitude, longitude, isFull, onDuty) {
+	cab = new google.maps.Marker({
 		map: map,
-		position: new google.maps.LatLng(32.986498, -96.751010),
+		position: new google.maps.LatLng(latitude, longitude),
 		icon: {
 		path: google.maps.SymbolPath.BACKWARD_CLOSED_ARROW,
 		fillOpacity: 1.0,
-		fillColor: '#008000',
+		fillColor: setCabColor(isFull, onDuty),
 		strokeOpacity: 1.0,
-		strokeColor: '#008000',
+		strokeColor: setCabColor(isFull, onDuty),
 		strokeWeight: 1.0,				
 		scale: 7 //pixels
-		//map: map
 		}		
-		}); 
-
-		
-
-	phase1SouthMarker = new google.maps.Marker({
-		map: map,
-		position: new google.maps.LatLng(32.985672, -96.754399),
-		icon: {
-		path: google.maps.SymbolPath.BACKWARD_CLOSED_ARROW,
-		fillOpacity: 1.0,
-		fillColor: '#FF0000',
-		strokeOpacity: 1.0,
-		strokeColor: '#FF0000',
-		strokeWeight: 1.0,	
-		rotation: -90,
-		scale: 7 //pixels
-		//map: map
-		}		
-		});
+		});   
   }
 
 	  /*Last line of code from my things*/
@@ -243,17 +272,4 @@ angular.module('starter', ['ionic'])
       };
       
     });
-/*The below is in the javascript by default. I left it there, but uncommenting it breaks the code */
-	/*
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
-})*/
+
